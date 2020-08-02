f0d7bfc8eb2bf3d6912ec535932820a2855f5846
Mark <markandrewhamm@gmail.com>
2020-08-02 13:02:24 -0400

    practice v-if, and v-for

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ var app = new Vue({
     message: "Hello from JavaScript!",
     firstName: "Mark",
     lastName: "Hamm",
-
+    info: false,
+    sports: ["ultimate frisbee", "disc golf", "other fake sports"],
+    additionalSport: ""
   },
   methods: {
     changeMessage: function () {
       console.log('changing greeting');
       this.message = "We're ready to go!";
+    },
+    toggleInfo: function () {
+      console.log('toggling info...');
+      this.info = !this.info;
+    },
+    addSport: function () {
+      console.log('adding sport...');
+      this.sports.push(this.additionalSport);
     }
   },
 });
